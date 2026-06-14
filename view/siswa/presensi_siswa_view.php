b537f93fb3b1b7ce5637b364f49be59eb9aa6d71
daffaharashtarizkypratama-commits <daffaharashtarizkypratama@gmail.com>
2026-06-14 16:26:26 +0700

    Memperbaiki fitur presensi siswa

--- view/siswa/presensi_siswa_view.php
+++ view/siswa/presensi_siswa_view.php
@@ -80,9 +80,9 @@ include '../../layout/sidebar_siswa.php';
 
                                 <?php if (!$status) : ?>
 
-                                    <a href="form_presensi_guru_view.php?presensi_id=<?= $presensi['id'] ?>"
+                                    <a href="form_presensi_siswa_view.php?presensi_id=<?= $presensi['id'] ?>"
                                         class="btn btn-primary rounded-3">
-                                        Presensi
+                                    Presensi
                                     </a>
 
                                 <?php else : ?>
